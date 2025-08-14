3e27e6cabb18ec5a9be5bcd93422106bf2e2d763
Mohamed El-Assy <39006475+3assy2018@users.noreply.github.com>
2025-08-14 03:26:02 +0200

    chore: bundle full countries and regions data

--- tests/Integration/CommandsTest.php
+++ tests/Integration/CommandsTest.php
@@ -11,22 +11,26 @@ class CommandsTest extends TestCase
     public function test_all_countries_command_seeds_countries()
     {
         $this->artisan('nationals:start')->assertExitCode(0);
-        $this->assertEquals(2, Country::count());
+        $data = json_decode(file_get_contents(__DIR__.'/../../database/data/countries.json'), true);
+        $this->assertEquals(count($data), Country::count());
     }
 
     public function test_add_regions_command_seeds_all_regions()
     {
         $this->artisan('nationals:start');
         $this->artisan('region:add', ['all' => '*'])->assertExitCode(0);
-        $this->assertEquals(4, Region::count());
+        $regionsData = json_decode(file_get_contents(__DIR__.'/../../database/data/regions.json'), true);
+        $total = array_reduce($regionsData, fn ($carry, $regions) => $carry + count($regions), 0);
+        $this->assertEquals($total, Region::count());
     }
 
     public function test_add_regions_command_seeds_specific_country()
     {
         $this->artisan('nationals:start');
+        $regionsData = json_decode(file_get_contents(__DIR__.'/../../database/data/regions.json'), true);
         $this->artisan('region:add', ['--code' => ['EG']])->assertExitCode(0);
-        $this->assertEquals(2, Region::count());
-        $this->assertEquals(2, Country::where('code', 'EG')->first()->regions()->count());
+        $this->assertEquals(count($regionsData['EG']), Region::count());
+        $this->assertEquals(count($regionsData['EG']), Country::where('code', 'EG')->first()->regions()->count());
         $this->assertEquals(0, Country::where('code', 'US')->first()->regions()->count());
     }
 }
